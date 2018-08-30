8ec3bf55c4953e55bf41a9d9d2badbf0a1c7c3a9
mukhibillah <mukhib.cascuser303@gmail.com>
2018-08-31 01:13:02 +0700

    all page InsyaAllah done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,6 +24,7 @@ class HomeController extends Controller
     public function index()
     {
         $data = "judul_buku_wkkw";
-        return view('detail',compact('data'));
+        return view('view',compact('data'));
+        // return view('');
     }
 }
